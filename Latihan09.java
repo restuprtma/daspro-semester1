4bad6c2bd3ec94a9943e7f2fdd6239bca5263876
restuprtma <restu.prtma05@gmail.com>
2023-10-04 14:40:31 +0700

    :fire:jobsheet daspro

---
+++ Latihan09.java
@@ -0,0 +1,11 @@
+/**
+ * Latihan09
+ */
+public class Latihan09 {
+                                                                                                                                                                                                                
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Fais Restu");
+        //System.out.print("\nSaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+
+    }
+}
---
+++ Latihan1.java
@@ -0,0 +1,15 @@
+/**
+ * Latihan1
+ */
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama     : Fais Restu Pratama");
+        System.out.println("Absen    : 9");
+        System.out.println("Kelas    : 1D");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("Jurusan  : Teknologi Informasi");
+        
+    }
+}
