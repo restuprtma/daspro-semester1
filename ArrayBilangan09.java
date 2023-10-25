c5619990c7a0de21136734085f9393b6a73e1b65
restuprtma <restu.prtma05@gmail.com>
2023-10-25 13:56:51 +0700

    Percobaan 1 array bilangan

---
+++ ArrayBilangan09.java
@@ -0,0 +1,12 @@
+package jobsheet9;
+
+public class ArrayBilangan09 {
+    
+    public static void main(String[] args) {
+        int [] bil = {5, 13, -7, 17};
+
+        for (int i = 0; 1 < 4; i++){
+        System.out.println(bil[i]);
+        }
+    }
+}
